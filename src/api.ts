7f29f0b33f6b27c1b1ea04125cecc0bbbd5206b8
Michel <michel.recchia3@etu.univ-lorraine.fr>
2025-03-19 23:00:46 +0100

    Added data

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,25 @@ import cors from 'cors';
 
 export const app = express();
 
+const jsonData = [
+  {
+      "name": "Starry Night",
+      "description": "A beautiful depiction of a starry night by Vincent van Gogh.",
+      "image": "https://drive.google.com/file/d/1COrN2fb8_Tcwc-3V9GERtLlhUwAte42l/view?usp=sharing"
+  },
+  {
+      "name": "Crying eye",
+      "description": "Filled with tears, this eye is a symbol of sadness and pain.",
+      "image": "https://drive.google.com/file/d/1TwZY5gnCyiB2MBEWxz6uC1HOcygNFakx/view?usp=sharing"
+  },
+  {
+      "name": "Blue medusa",
+      "description": "Two blue medasas with a white background.",
+      "image": "https://drive.google.com/file/d/1P3ogrdrvP4SbLvjZHrx7Oj9JXcIJxNsi/view?usp=sharing"
+  }
+];
+
+
 app.use(cors({ origin: true }));
 
 app.use(express.json());
@@ -14,11 +33,16 @@ app.get('/', (req, res) => {
   res.status(200).send({ status: 'ok' });
 });
 
-const api = express.Router();
-
-api.get('/hello', (req, res) => {
-  res.status(200).send({ message: 'hello world' });
+app.get("/:id" , (req, res) => {
+  const id = parseInt(req.params.id);
+  if (jsonData[id]) {
+      res.json(jsonData[id]);
+  } else {
+      res.status(404).send("NFT not found");
+  }
 });
+
+const api = express.Router();
 
 // Version the api
 app.use('/api/v1', api);
